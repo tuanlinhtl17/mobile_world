cfcff4f5136e9826f3690149f35b733a6a42d1aa
tuanlinhtl17 <tuanlinhtl17@gmail.com>
2018-02-28 21:06:43 +0700

    login_logout
    login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   scope "(:locale)", locale: /en|vi/ do
     post "/rate" => "rater#create", :as => "rate"
-    devise_for :users
     root to: "pages#index"
     resources :mobiles
     resources :makers
   end
+  
+  devise_for :users, controllers: { omniauth_callbacks: "user/omniauth_callbacks" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228121852) do
+ActiveRecord::Schema.define(version: 20180228124325) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer "rater_id"
@@ -156,6 +156,16 @@ ActiveRecord::Schema.define(version: 20180228121852) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "provider"
+    t.string "uid"
+    t.string "image"
+    t.integer "failed_attempts", default: 0
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
