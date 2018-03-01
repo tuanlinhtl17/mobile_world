bd88088f5d8f027ba3bf5c590f05f1dd583938a2
huuphuc93 <huuphuc.ns@gmail.com>
2018-03-01 23:12:19 +0700

    add teamplates homepages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     post "/rate" => "rater#create", :as => "rate"
     devise_for :users
-    root to: "home#index"
+    root to: "pages#index"
   end
 end
